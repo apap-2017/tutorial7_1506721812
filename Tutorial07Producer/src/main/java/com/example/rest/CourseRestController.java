99f16b3dcdcdc13e80d70eb8b782201c94d93fe1
atikahzh <thika.zahrah@gmail.com>
2017-11-04 14:44:46 +0700

    selesai producer

---
+++ Tutorial07Producer/src/main/java/com/example/rest/CourseRestController.java
@@ -0,0 +1,30 @@
+package com.example.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.CourseModel;
+import com.example.service.StudentService;
+
+@RestController
+@RequestMapping("/rest")
+public class CourseRestController {
+	@Autowired
+	StudentService studentService;
+	
+	@RequestMapping("/course/view/{id}")
+	public CourseModel view(@PathVariable(value = "id") String id) {
+		CourseModel course = studentService.selectCourse(id);
+		return course;
+	}
+	
+	@RequestMapping("/course/viewall")
+	public List<CourseModel> viewAll() {
+		List<CourseModel> courses = studentService.selectAllCourse();
+		return courses;
+	}
+}
